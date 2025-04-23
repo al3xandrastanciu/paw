347ff2bf890433cd89fa2350d77e018383fa090b
al3xandrastanciu <alexandra.sa.35@gmail.com>
2025-04-23 22:11:24 +0300

    Add files via upload

---
+++ subiect_1048/FormVizualizare.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace subiect_1048
+{
+    partial class FormVizualizare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stergeElementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvareXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(389, 373);
+            this.treeView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stergeElementToolStripMenuItem,
+            this.salvareXMLToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // stergeElementToolStripMenuItem
+            // 
+            this.stergeElementToolStripMenuItem.Name = "stergeElementToolStripMenuItem";
+            this.stergeElementToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.stergeElementToolStripMenuItem.Text = "Sterge element";
+            this.stergeElementToolStripMenuItem.Click += new System.EventHandler(this.stergeElementToolStripMenuItem_Click);
+            // 
+            // salvareXMLToolStripMenuItem
+            // 
+            this.salvareXMLToolStripMenuItem.Name = "salvareXMLToolStripMenuItem";
+            this.salvareXMLToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salvareXMLToolStripMenuItem.Text = "Salvare XML";
+            this.salvareXMLToolStripMenuItem.Click += new System.EventHandler(this.salvareXMLToolStripMenuItem_Click);
+            // 
+            // FormVizualizare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 373);
+            this.Controls.Add(this.treeView1);
+            this.Name = "FormVizualizare";
+            this.Text = "FormVizualizare";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem stergeElementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvareXMLToolStripMenuItem;
+    }
+}
---
+++ subiect_1048/Rezervare.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subiect_1048
+{
+    [Serializable]
+    public class Rezervare
+    {
+        private String nume;
+        DateTime dataCheckIn;
+        int numarNopti;
+        private decimal pret;
+        public bool isAchitat;
+
+        public Rezervare(string nume, DateTime dataCheckIn, int numarNopti, decimal pret, bool achitat = false)
+        {
+            this.nume = nume;
+            this.dataCheckIn = dataCheckIn;
+            this.numarNopti = numarNopti;
+            this.pret = pret;
+            this.isAchitat = achitat;
+        }
+        public void setNume(String n) { nume = n; }
+        public string getNume() { return nume; }
+        public void setPret(decimal p) { pret = p; }
+        public decimal getPret() { return pret; }
+        public override string ToString()
+        {
+            return $"{nume}-{dataCheckIn.ToShortDateString()}-{numarNopti}-{pret}";
+        }
+    }
+}
